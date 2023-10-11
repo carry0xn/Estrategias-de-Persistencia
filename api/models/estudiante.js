e60103df0c1a5b40cbb66aadf8db7d06b65367c8
Cristian Juarez <cristianjuarezdev@gmail.com>
2023-10-11 18:43:51 -0300

    Model & migration (?)

---
+++ api/models/estudiante.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const estudiante = sequelize.define('estudiante', {
+    nombre: DataTypes.STRING,
+    apellido: DataTypes.STRING,
+    dni: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    }
+  }, {});
+
+  estudiante.associate = function (models){
+    estudiante.hasMany(models.materia)
+  }
+  
+  return profesor;
+};
